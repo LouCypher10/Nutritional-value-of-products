5410f8e59e88cf21712c452d8df4eb4aea4b6612
Lou Cypher <98168195+LouCypher10@users.noreply.github.com>
2022-05-18 19:23:16 +0200

    Add a new products

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-function Product(kcal, protein, fat, carbohydrate, fiber){
+function Product(name, kcal, protein, fat, carbohydrate, fiber){
+    this.name = name;
     this.kcal = kcal;
     this.protein = protein;
     this.fat = fat;
     this.carbohydrate = carbohydrate;
     this.fiber = fiber;
-}
+};
+
+const mp500 = new Product("Mąka pszenna typ 500", 343, 10.10, 1.20, 74, 2.30);
+const mp550 = new Product("Mąka pszenna typ 550", 344, 10.50, 1.60, 73, 2.30);
