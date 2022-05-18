3ba101a4d2c0143a7684c6f5c58150b72bee7339
Lou Cypher <98168195+LouCypher10@users.noreply.github.com>
2022-05-18 20:15:52 +0200

    add new div

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-function Product(name, kcal, protein, fat, carbohydrate, fiber){
-    this.name = name;
-    this.kcal = kcal;
-    this.protein = protein;
-    this.fat = fat;
-    this.carbohydrate = carbohydrate;
-    this.fiber = fiber;
-};
+function Product(name, kcal, protein, fat, carbohydrate, fiber) {
+  this.name = name;
+  this.kcal = kcal;
+  this.protein = protein;
+  this.fat = fat;
+  this.carbohydrate = carbohydrate;
+  this.fiber = fiber;
+}
 
-const mp500 = new Product("Mąka pszenna typ 500", 343, 10.10, 1.20, 74, 2.30);
-const mp550 = new Product("Mąka pszenna typ 550", 344, 10.50, 1.60, 73, 2.30);
+//Tworzenie nowych produktów = nazwa, kaloryczność, białko, tłuszcz, węglowodany, błonnik.
+const mp500 = new Product("Mąka pszenna typ 500", 343, 10.1, 1.2, 74, 2.3);
+const mp550 = new Product("Mąka pszenna typ 550", 344, 10.5, 1.6, 73, 2.3);
+
+let product = [mp500, mp550];
+
+container = document.getElementById("container");
+
+let el = document.createElement("span");
+
+product.forEach(function (item) {
+
+  container.innerHTML += "produkt: " + item.name + "<br>";
+});
